5ab0200efd6c232a5416ec089000abf890f27b9d
YeonGyu-Kim <code.yeon.gyu@gmail.com>
2026-04-28 19:13:04 +0900

    feat(simulator): page assembly with map+3D+charts+results+timeline (#47)
    T25. SimulatorPage combining ImpactMap, Asteroid3D, ResultsPanel, TimelineCards, charts. /simulator route.
    
    Co-authored-by: Dionysus <dionysus@olympus.team>

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { useRegisterSW } from './registerSW';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import LandingPage from './pages/landing/LandingPage';
+import SimulatorPage from './pages/simulator/SimulatorPage';
 
 export default function App() {
   useRegisterSW();
 
-  return <LandingPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/simulator" element={<SimulatorPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
